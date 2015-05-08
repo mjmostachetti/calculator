98170060bd4feeaee709e9ca650fc2e7a75f17e2
sjellis <stephenjohnellis@gmail.com>
2015-05-08 09:24:48 -0700

    Made it Awesome

--- diff_cal.js
+++ diff_cal.js
@@ -7,30 +7,33 @@ for(var i = 0; process.argv.length > i; i++){
 	for(var x = 0; newArray.length > x; x++){
 		if(newArray[x] === '*'){
 			operand = x;
-			break;
 		}else if(newArray[x] === '/'){
 			operand = x;
-			break;
-		}else if(newArray[x] === '+'){
+		}
+
+		if(newArray[operand] === '*'){
+				var newMult = newArray[operand-1] * newArray[operand+1];
+				newArray.splice(operand-1,3,newMult);
+		}else if(newArray[operand] === '/'){
+				var newDiv = newArray[operand-1] / newArray[operand+1];
+				newArray.splice(operand-1,3,newDiv);
+	}
+}
+
+	for(var x = 0; newArray.length > x; x++){
+		if(newArray[x] === '+'){
 			operand = x;
-			break;
 		}else if(newArray[x] === '-'){
 			operand = x;
 		}
-	}
 
-	if(newArray[operand] === '*'){
-			var newMult = newArray[operand-1] * newArray[operand+1];
-			newArray.splice(operand-1,3,newMult);
-	}else if(newArray[operand] === '/'){
-			var newDiv = newArray[operand-1] / newArray[operand+1];
-			newArray.splice(operand-1,3,newDiv);
-	}else if(newArray[operand] === '+'){
-			var newAdd = newArray[operand-1] + newArray[operand+1];
+	if(newArray[operand] === '+'){
+			var newAdd = parseInt(newArray[operand-1],10) + parseInt(newArray[operand+1],10);
 			newArray.splice(operand-1,3,newAdd);
 	}else if(newArray[operand] === '-'){
 			var newSub = newArray[operand-1] - newArray[operand+1];
 			newArray.splice(operand-1,3,newSub);
+		}
 	}
 }
 
